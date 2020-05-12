aad4b129cd9d56f2b1509d80f8277d297b241646
dviedma <daniel.viedma@wondersauce.com>
2020-05-12 08:31:05 -0400

    Adding react example + disabling browsersync auto reload

--- src/js/test.module.js
+++ src/js/test.module.js
@@ -1,8 +1,24 @@
 import React from "react";
+import ReactDOM from "react-dom";
+
+class HelloMessage extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <h1>Hello {this.props.name}</h1>
+        </div>
+      </div>
+    );
+  }
+}
 
 const TestModule = {
   init: () => {
-    render(<h1>Hello world</h1>, select("body"));
+    let App = document.getElementById("app");
+    if (App) {
+      ReactDOM.render(<HelloMessage name="Yomi" />, App);
+    }
   },
 };
 
